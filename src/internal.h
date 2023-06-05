4625f2eb832d519abcccf3ce77101c0f5b7409fc
Christopher Bohn <bohn@unl.edu>
2023-06-04 22:33:09 -0500

    Migrated MAX7219 code from CowPi to CowPi_stdio

---
+++ src/internal.h
@@ -0,0 +1,40 @@
+/**************************************************************************//**
+ *
+ * @file cowpi_stdio_internal.h
+ *
+ * @author Christopher A. Bohn
+ *
+ * @brief Functions, constants, and externalized variables that are not meant
+ *      for use by application code.
+ *
+ ******************************************************************************/
+
+/* CowPi_stdio (c) 2022-23 Christopher A. Bohn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef COWPI_STDIO_INTERNAL_H
+#define COWPI_STDIO_INTERNAL_H
+
+#include "typedefs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cowpi_setup_max7219(const cowpi_display_module_protocol_t *configuration);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif //COWPI_STDIO_INTERNAL_H
---
+++ src/typedefs.h
@@ -0,0 +1,53 @@
+/**************************************************************************//**
+ *
+ * @file typedefs.h
+ *
+ * @author Christopher A. Bohn
+ *
+ * @brief Structures that consolidate configuration data for display modules
+ *      and protocols to communication with the display modules
+ *
+ ******************************************************************************/
+
+/* CowPi_stdio (c) 2022-23 Christopher A. Bohn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef COWPI_STDIO_TYPEDEFS_H
+#define COWPI_STDIO_TYPEDEFS_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Structure that consolidates all information needed to determine how data
+ * should be sent to a display module.
+ */
+typedef struct {
+    enum {
+        UNSPECIFIED = 0,
+        I2C,
+        SPI
+    } protocol;
+    uint8_t data_pin;
+    uint8_t clock_pin;
+    uint8_t select_pin;
+} cowpi_display_module_protocol_t;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif //COWPI_STDIO_TYPEDEFS_H
